db642d9f27b7d442b4134359b944d05d4d92158b
ravis <ravis@DESKTOP-MO95S8H>
2020-09-27 07:39:44 +0530

    Add project files.

---
+++ Tech Project 3B- Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Project_3B__Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
